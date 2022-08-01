b362eea45262d0821e71884b4e0763adc24c8126
emiriPocke <nakamori.emiri@pocke.co.jp>
2022-08-01 23:25:40 +0900

    testCommit

---
+++ SwiftChartsTest/SwiftChartsTest Watch App/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  SwiftChartsTest Watch App
+//
+//  Created by nakamori on 2022/08/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftChartsTest/SwiftChartsTest/SwiftChartsTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftChartsTestApp.swift
+//  SwiftChartsTest
+//
+//  Created by nakamori on 2022/08/01.
+//
+
+import SwiftUI
+
+@main
+struct SwiftChartsTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
